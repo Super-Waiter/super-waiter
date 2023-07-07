10b2a5f9b2c0f9c69278ba717092e31270bee6c2
Sardorbek <sardorbekaminjonov2001@mail.ru>
2023-07-07 06:01:37 +0500

    Install react-native-vision-camera

--- src/navigation/RootStack.tsx
+++ src/navigation/RootStack.tsx
@@ -13,7 +13,7 @@ const RootStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Waiter"
+        initialRouteName="Scanner"
         screenOptions={{
           headerShown: false,
         }}>
--- src/screens/main/scanner/index.tsx
+++ src/screens/main/scanner/index.tsx
@@ -1,14 +1,56 @@
-import {ScrollView, StyleSheet} from 'react-native';
-import React from 'react';
+import {Alert, Linking, ScrollView, StyleSheet, View} from 'react-native';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AppHeader} from '../../../components/AppHeader';
 import {useNavigation} from '@react-navigation/native';
 import {ScannerScreenNavigationProp} from '../../../navigation/types';
 import {AppButton} from '../../../components/AppButton';
-import {Colors} from '../../../style';
+import {Colors, Utils} from '../../../style';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import {LoadingBackdrop} from '../../../components/LoadingBackdrop';
 
 const ScannerScreen = () => {
   const navigation = useNavigation<ScannerScreenNavigationProp>();
+
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  useEffect(() => {
+    const handleCameraPermission = async () => {
+      try {
+        const cameraPermission = await Camera.getCameraPermissionStatus();
+
+        if (cameraPermission === 'not-determined') {
+          await Camera.requestCameraPermission();
+        } else if (cameraPermission === 'denied') {
+          Alert.alert(
+            'Camera permission',
+            'Please enable camera access in your settings',
+            [
+              {
+                text: 'Cancel',
+                style: 'cancel',
+              },
+              {
+                text: 'Open settings',
+                onPress: () => Linking.openSettings(),
+              },
+            ],
+          );
+        } else if (cameraPermission === 'authorized') {
+          console.log('authorized');
+        } else if (cameraPermission === 'restricted') {
+          console.log('restricted');
+        } else {
+          console.log('unknown');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    handleCameraPermission();
+  }, [device]);
 
   const handleGoToWaiter = () => {
     navigation.navigate('Waiter');
@@ -17,6 +59,10 @@ const ScannerScreen = () => {
     navigation.navigate('Client');
   };
 
+  if (device == null) {
+    return <LoadingBackdrop />;
+  }
+
   return (
     <SafeAreaView style={styles.main} edges={['top']}>
       <AppHeader
@@ -24,6 +70,14 @@ const ScannerScreen = () => {
         titleColor={Colors.White}
         backgroundColor={Colors.Primary}
       />
+
+      <View style={styles.cameraView}>
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+        />
+      </View>
 
       <ScrollView>
         <AppButton title="Client" onPress={handleGoToWaiter} />
@@ -36,6 +90,11 @@ const ScannerScreen = () => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
+  },
+  cameraView: {
+    width: Utils.DEVICE_WIDTH * 0.9,
+    height: Utils.DEVICE_WIDTH * 0.9,
+    margin: Utils.DEVICE_WIDTH * 0.05,
   },
 });
 
